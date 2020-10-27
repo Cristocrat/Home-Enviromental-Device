f70c12c1f3d608e3b975eb2cf401c0ad67508941
Cristocrat <Cristocrat@users.noreply.github.com>
2020-10-27 02:25:37 -0500

    Update main.dart

--- home_enviroment_device/lib/main.dart
+++ home_enviroment_device/lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(
-        title: 'Home Enviroment Device',
+        title: 'Home',
       ),
     );
   }
