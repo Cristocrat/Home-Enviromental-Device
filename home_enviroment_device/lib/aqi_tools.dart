853e9d2e1092a77a0aca40481e55245aacc31c88
Cristocrat <Cristocrat@users.noreply.github.com>
2020-10-27 04:31:26 -0500

    fixed the color of the aqi and added the title of it

--- home_enviroment_device/lib/aqi_tools.dart
+++ home_enviroment_device/lib/aqi_tools.dart
@@ -8,17 +8,20 @@ class AqiTools {
     value = 0;
   }
 
+  get api => null;
+
   set currentApiValue(int n) {
     value = n;
   }
 
-  Color getColor(item) {
-    if (item >= 50) return Colors.green;
-
+  static Color getColor(int item) {
+    if (item <= 50) return Colors.green;
     if (item < 50 && item >= 100) return Colors.yellow;
     if (item < 100 && item >= 150) return Colors.orange;
     if (item < 150 && item >= 200) return Colors.red;
-    if (item < 200 && item >= 250) return Colors.purple;
-    if (item < 300) return Colors.brown;
+    if (item < 200 && item >= 250)
+      return Colors.purple;
+    else
+      return Colors.black;
   }
 }
--- home_enviroment_device/lib/mainScreen.dart
+++ home_enviroment_device/lib/mainScreen.dart
@@ -5,7 +5,7 @@ import 'aqi_tools.dart';
 
 // ignore: camel_case_types
 class mainScreen extends StatefulWidget {
-  
+  AqiTools aqivalue = new AqiTools(" ");
   @override
   _mainScreenState createState() => _mainScreenState();
 }
@@ -13,11 +13,13 @@ class mainScreen extends StatefulWidget {
 class _mainScreenState extends State<mainScreen> {
   @override
   Widget build(BuildContext context) {
-    int api = 32;
+    AqiTools aqivalue;
+    int aqi = 32;
+    widget.aqivalue.value = aqi;
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(icon: Icon(Icons.menu), onPressed: null),
-        title: Text('logo'),
+        title: Text('name'),
         centerTitle: true,
         actions: <Widget>[
           IconButton(icon: Icon(Icons.person_outline), onPressed: null)
@@ -28,22 +30,33 @@ class _mainScreenState extends State<mainScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Container(
-              height: 200,
-              width: 300,
-              color: Colors.transparent,
-              child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      border: Border.all()),
-                  child: new Center(
-                    child: new Text(
-                      "$api",
-                      style: TextStyle(color: Colors.white, fontSize: 22),
-                      textAlign: TextAlign.center,
-                    ),
-                  )),
-            )
+                height: 200,
+                width: 300,
+                color: Colors.transparent,
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        border: Border.all()),
+                    child: new Center(
+                        child: ListView(children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 13, 0, 13),
+                        child: new Text(
+                          "Air Quality Index",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Padding(
+                          child: new Text(
+                            "$aqi",
+                            style: TextStyle(
+                                color: AqiTools.getColor(widget.aqivalue.value),
+                                fontSize: 22),
+                            textAlign: TextAlign.center,
+                          ),
+                          padding: EdgeInsets.fromLTRB(0, 0, 0, 0)),
+                    ])))),
           ],
         ),
       ),
